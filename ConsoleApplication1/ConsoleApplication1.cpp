2fe0a80ea270351b1ab3f303433da83af813f208
Ciprian Badescu <c@b.a>
2020-06-12 12:10:34 +0000

    initial

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,253 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+extern "C" {
+    #include "Header1.h"
+}
+
+
+
+//typedef struct _CLIENT_ID
+//{
+//    HANDLE UniqueProcess;
+//    HANDLE UniqueThread;
+//} CLIENT_ID, * PCLIENT_ID;
+
+void ErrorExit(const wchar_t * lpszFunction)
+{
+    // Retrieve the system error message for the last-error code
+
+    LPVOID lpMsgBuf;
+    LPVOID lpDisplayBuf;
+    DWORD dw = GetLastError();
+
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        dw,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf,
+        0, NULL);
+
+    // Display the error message and exit the process
+
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+        (lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+    std::wcout << lpszFunction << TEXT(": ") << (LPCTSTR)lpMsgBuf;
+
+
+    //MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
+
+    LocalFree(lpMsgBuf);
+    LocalFree(lpDisplayBuf);
+    /*int a;
+    std::cin >> a;*/
+    ExitProcess(dw);
+}
+
+
+DWORD GetServicePid(const char* name)
+{
+    SC_HANDLE theService, scm;
+    SERVICE_STATUS m_SERVICE_STATUS;
+    SERVICE_STATUS_PROCESS ssStatus;
+    DWORD dwBytesNeeded;
+
+
+    scm = OpenSCManager(nullptr, nullptr, SC_MANAGER_ENUMERATE_SERVICE);
+    if (!scm) {
+        return 0;
+    }
+
+    theService = OpenServiceA(scm, name, SERVICE_QUERY_STATUS);
+    if (!theService) {
+        CloseServiceHandle(scm);
+        return 0;
+    }
+
+    auto result = QueryServiceStatusEx(theService, SC_STATUS_PROCESS_INFO,
+        reinterpret_cast<LPBYTE>(&ssStatus), sizeof(SERVICE_STATUS_PROCESS),
+        &dwBytesNeeded);
+
+    CloseServiceHandle(theService);
+    CloseServiceHandle(scm);
+
+    if (result == 0) {
+        return 0;
+    }
+
+    return ssStatus.dwProcessId;
+}
+
+//void GetProcessMaps(DWORD pid) {
+//    auto processHandler = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, true, pid);
+//    if (processHandler == NULL) {
+//        std::cout << "Function failed \n";
+//        ErrorExit(LPTSTR("GetProcessID"));
+//    }
+//   
+//}
+
+void getFilename(HANDLE hProcess, ULONG_PTR allocationBase, PUNICODE_STRING pattern)
+{
+    SIZE_T bufferSize;
+    SIZE_T returnLength;
+    PUNICODE_STRING buffer;
+
+    bufferSize = 0x100;
+    buffer = (PUNICODE_STRING)LocalAlloc(LPTR, bufferSize);
+    NTSTATUS status;
+
+    status = NtQueryVirtualMemory(
+        hProcess,
+        (PVOID)allocationBase,
+        MemoryMappedFilenameInformation,
+        buffer,
+        bufferSize,
+        &returnLength
+    );
+
+    if (status == STATUS_BUFFER_OVERFLOW)
+    {
+        LocalFree(buffer);
+        bufferSize = returnLength;
+        buffer = (PUNICODE_STRING)LocalAlloc(LPTR, bufferSize);
+
+        status = NtQueryVirtualMemory(
+            hProcess,
+            (PVOID)allocationBase,
+            MemoryMappedFilenameInformation,
+            buffer,
+            bufferSize,
+            &returnLength
+        );
+    }
+    if (status > 0)
+    {
+        LocalFree(buffer);
+        ErrorExit(TEXT("NtQueryVirtualMemory Getfilename"));
+    }
+    if (buffer->Length != 0) {
+        printf("Filename: %wZ\n", buffer);
+        
+        //compareFilenameToContain
+    }
+    
+    LocalFree(buffer);
+
+
+}
+
+int PrintModules(DWORD processID)
+{
+    HMODULE hMods[1024];
+    HANDLE hProcess;
+    DWORD cbNeeded;
+    unsigned int i;
+
+    // Print the process identifier.
+
+    printf("\nProcess ID: %u\n", processID);
+
+    // Get a handle to the process.
+
+    hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |
+        PROCESS_VM_READ,
+        FALSE, processID);
+    if (NULL == hProcess) {
+        ErrorExit(TEXT("OpenProcess"));
+    }
+
+    printf("\nlisting all modules\n");
+    // Get a list of all the modules in this process.
+    //#define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
+
+    ULONG_PTR baseAddress(0);
+    MEMORY_BASIC_INFORMATION basicInfo;
+
+    if (NtQueryVirtualMemory(
+        hProcess,
+        (PVOID) baseAddress,
+        MemoryBasicInformation,
+        &basicInfo,
+        sizeof(MEMORY_BASIC_INFORMATION),
+        NULL
+    ) > 0)
+    {
+        ErrorExit(TEXT("NtQueryVirtualMemory 1"));
+    }
+    BOOLEAN keepReading = TRUE;
+    ULONG_PTR allocationBase;
+    
+    baseAddress = (ULONG_PTR)basicInfo.AllocationBase;
+    std::cout << "initial baseAddress: 0x" << std::hex << baseAddress << std::endl;
+    while (keepReading) {
+        //char c;
+        //std::cin.get(c);
+        if (basicInfo.Type == MEM_MAPPED || basicInfo.Type == MEM_IMAGE) {
+            //std::cout << "MEMMAPPED or IMAGE" << std::endl;
+            allocationBase = (ULONG_PTR) basicInfo.AllocationBase;
+            do {
+                baseAddress = baseAddress + basicInfo.RegionSize;
+                //std::cout << "RegionSize: 0x" << std::hex << basicInfo.RegionSize << std::endl;
+                //std::cout << "baseAddress: 0x" << std::hex << baseAddress << std::endl;
+                if (NtQueryVirtualMemory(
+                    hProcess,
+                    (PVOID) baseAddress,
+                    MemoryBasicInformation,
+                    &basicInfo,
+                    sizeof(MEMORY_BASIC_INFORMATION),
+                    NULL
+                ) < 0) {
+                    ErrorExit(TEXT("NtQueryVirtualMemory 2"));
+                }
+            } while ((ULONG_PTR)basicInfo.AllocationBase == allocationBase);
+            //FIXME to get filename
+            std::cout << "module_base_address: " << allocationBase << std::endl;
+            UNICODE_STRING someStr;
+            //RtlInitUnicodeString(&someStr, L"Hello");
+            getFilename(hProcess, allocationBase, &someStr);
+        }
+        else {
+            //std::cout << "NOT MEMMAPPED or IMAGE" << std::endl;
+            baseAddress = baseAddress + basicInfo.RegionSize;
+            //std::cout << "RegionSize: 0x" << std::hex << basicInfo.RegionSize << std::endl;
+            //std::cout << "baseAddress: 0x" << std::hex << baseAddress << std::endl;
+            if (NtQueryVirtualMemory(
+                hProcess,
+                (PVOID)baseAddress,
+                MemoryBasicInformation,
+                &basicInfo,
+                sizeof(MEMORY_BASIC_INFORMATION),
+                NULL
+            ) < 0) {
+                ErrorExit(TEXT("NtQueryVirtualMemory 3"));
+            }
+
+        }
+
+    }
+    
+
+    // Release the handle to the process.
+    CloseHandle(hProcess);
+
+    return 0;
+}
+
+
+
+int main()
+{
+    auto processID = GetServicePid("EventLog");
+    if (processID != 0) {
+        std::cout << "service found: Process ID " << processID << "\n";
+        PrintModules(processID);
+    }
+    else {
+        std::cout << "Service not found\n";
+    }
+}
---
+++ ConsoleApplication1/Header1.h
@@ -0,0 +1,44 @@
+#pragma once
+#pragma comment(lib, "ntdll.lib")
+
+#include <windows.h>
+#include <windowsx.h>
+
+
+typedef enum _MEMORY_INFORMATION_CLASS
+{
+    MemoryBasicInformation, // MEMORY_BASIC_INFORMATION
+    MemoryWorkingSetInformation, // MEMORY_WORKING_SET_INFORMATION
+    MemoryMappedFilenameInformation, // UNICODE_STRING
+    MemoryRegionInformation, // MEMORY_REGION_INFORMATION
+    MemoryWorkingSetExInformation, // MEMORY_WORKING_SET_EX_INFORMATION
+    MemorySharedCommitInformation, // MEMORY_SHARED_COMMIT_INFORMATION
+    MemoryImageInformation, // MEMORY_IMAGE_INFORMATION
+    MemoryRegionInformationEx, // MEMORY_REGION_INFORMATION
+    MemoryPrivilegedBasicInformation,
+    MemoryEnclaveImageInformation, // MEMORY_ENCLAVE_IMAGE_INFORMATION // since REDSTONE3
+    MemoryBasicInformationCapped, // 10
+    MemoryPhysicalContiguityInformation, // MEMORY_PHYSICAL_CONTIGUITY_INFORMATION // since 20H1
+    MaxMemoryInfoClass
+} MEMORY_INFORMATION_CLASS;
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtQueryVirtualMemory(
+    _In_ HANDLE ProcessHandle,
+    _In_opt_ PVOID BaseAddress,
+    _In_ MEMORY_INFORMATION_CLASS MemoryInformationClass,
+    _Out_writes_bytes_(MemoryInformationLength) PVOID MemoryInformation,
+    _In_ SIZE_T MemoryInformationLength,
+    _Out_opt_ PSIZE_T ReturnLength
+);
+
+#define STATUS_BUFFER_OVERFLOW           ((NTSTATUS)0x80000005L)
+
+typedef struct _UNICODE_STRING
+{
+    USHORT Length;
+    USHORT MaximumLength;
+    _Field_size_bytes_part_(MaximumLength, Length) PWCH Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
